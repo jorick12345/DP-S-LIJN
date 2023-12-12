ed564bae67db9c0292dc65e70cb65cec5928eaeb
Jorick <jorick.vanderhave@student.hu.nl>
2023-12-12 17:31:38 +0100

    S5 en S6

--- opdracht/S5_Subqueries_student.sql
+++ opdracht/S5_Subqueries_student.sql
@@ -31,51 +31,58 @@
 -- S5.1.
 -- Welke medewerkers hebben zowel de Java als de XML cursus
 -- gevolgd? Geef hun personeelsnummers.
--- DROP VIEW IF EXISTS s5_1; CREATE OR REPLACE VIEW s5_1 AS                                                     -- [TEST]
-
+DROP VIEW IF EXISTS s5_1; CREATE OR REPLACE VIEW s5_1 AS                                                     -- [TEST]
+select mnr from medewerkers where mnr in (select cursist from inschrijvingen where cursus = 'JAV' ) and mnr in(select cursist from inschrijvingen where cursus = 'XML');
 
 -- S5.2.
 -- Geef de nummers van alle medewerkers die niet aan de afdeling 'OPLEIDINGEN'
 -- zijn verbonden.
--- DROP VIEW IF EXISTS s5_2; CREATE OR REPLACE VIEW s5_2 AS                                                     -- [TEST]
-
+DROP VIEW IF EXISTS s5_2; CREATE OR REPLACE VIEW s5_2 AS                                                     -- [TEST]
+select mnr from medewerkers where mnr not in (select mnr from medewerkers where afd = 20);
 
 -- S5.3.
 -- Geef de nummers van alle medewerkers die de Java-cursus niet hebben
 -- gevolgd.
--- DROP VIEW IF EXISTS s5_3; CREATE OR REPLACE VIEW s5_3 AS                                                     -- [TEST]
-
+DROP VIEW IF EXISTS s5_3; CREATE OR REPLACE VIEW s5_3 AS                                                     -- [TEST]
+select mnr from medewerkers where mnr not in (select cursist from inschrijvingen where cursus='JAV');
 
 -- S5.4.
 -- a. Welke medewerkers hebben ondergeschikten? Geef hun naam.
--- DROP VIEW IF EXISTS s5_4a; CREATE OR REPLACE VIEW s5_4a AS                                                   -- [TEST]
-
+DROP VIEW IF EXISTS s5_4a; CREATE OR REPLACE VIEW s5_4a AS                                                   -- [TEST]
+select naam from medewerkers where mnr in (select chef from medewerkers);
 -- b. En welke medewerkers hebben geen ondergeschikten? Geef wederom de naam.
--- DROP VIEW IF EXISTS s5_4b; CREATE OR REPLACE VIEW s5_4b AS                                                   -- [TEST]
+DROP VIEW IF EXISTS s5_4b; CREATE OR REPLACE VIEW s5_4b AS                                                   -- [TEST]
+select naam from medewerkers where mnr not in (select chef from medewerkers where chef is not null);
 
 
 -- S5.5.
 -- Geef cursuscode en begindatum van alle uitvoeringen van programmeercursussen
 -- ('BLD') in 2020.
--- DROP VIEW IF EXISTS s5_5; CREATE OR REPLACE VIEW s5_5 AS                                                     -- [TEST]
-
+DROP VIEW IF EXISTS s5_5; CREATE OR REPLACE VIEW s5_5 AS                                                     -- [TEST]
+select u.cursus,u.begindatum from uitvoeringen u where u.begindatum between '2019-12-31' and '2020-12-31' and u.cursus in (select c.code from cursussen c where c.type ='BLD');
 
 -- S5.6.
 -- Geef van alle cursusuitvoeringen: de cursuscode, de begindatum en het
 -- aantal inschrijvingen (`aantal_inschrijvingen`). Sorteer op begindatum.
--- DROP VIEW IF EXISTS s5_6; CREATE OR REPLACE VIEW s5_6 AS                                                     -- [TEST]
-
+DROP VIEW IF EXISTS s5_6; CREATE OR REPLACE VIEW s5_6 AS                                                     -- [TEST]
+SELECT u.cursus, u.begindatum,
+       (SELECT COUNT(cursus) as aantal_inschrijvingen
+        FROM inschrijvingen
+        WHERE cursus = u.cursus AND begindatum = u.begindatum)
+FROM uitvoeringen u
+ORDER BY u.begindatum;
 
 -- S5.7.
 -- Geef voorletter(s) en achternaam van alle trainers die ooit tijdens een
 -- algemene ('ALG') cursus hun eigen chef als cursist hebben gehad.
--- DROP VIEW IF EXISTS s5_7; CREATE OR REPLACE VIEW s5_7 AS                                                     -- [TEST]
-
+DROP VIEW IF EXISTS s5_7; CREATE OR REPLACE VIEW s5_7 AS                                                     -- [TEST]
+select m.voorl,m.naam from medewerkers m where m.functie= 'TRAINER' and m.chef in
+(select i.cursist from inschrijvingen i,cursussen c,uitvoeringen u where i.cursus = c.code and c.type = 'ALG' and u.docent = m.mnr and i.begindatum = u.begindatum) ;
 
 -- S5.8.
 -- Geef de naam van de medewerkers die nog nooit een cursus hebben gegeven.
--- DROP VIEW IF EXISTS s5_8; CREATE OR REPLACE VIEW s5_8 AS                                                     -- [TEST]
-
+DROP VIEW IF EXISTS s5_8; CREATE OR REPLACE VIEW s5_8 AS                                                     -- [TEST]
+select naam from medewerkers where mnr not in (select docent from uitvoeringen where docent is not null);
 
 
 -- -------------------------[ HU TESTRAAMWERK ]--------------------------------
--- opdracht/S6_Views_student.sql
+++ opdracht/S6_Views_student.sql
@@ -12,18 +12,31 @@
 --
 -- 1. Maak een view met de naam "deelnemers" waarmee je de volgende gegevens uit de tabellen inschrijvingen en uitvoering combineert:
 --    inschrijvingen.cursist, inschrijvingen.cursus, inschrijvingen.begindatum, uitvoeringen.docent, uitvoeringen.locatie
+CREATE VIEW deelnemers AS
+SELECT inschrijvingen.cursist, inschrijvingen.cursus, inschrijvingen.begindatum, uitvoeringen.docent, uitvoeringen.locatie
+FROM inschrijvingen JOIN uitvoeringen on inschrijvingen.cursus = uitvoeringen.cursus;
 -- 2. Gebruik de view in een query waarbij je de "deelnemers" view combineert met de "personeels" view (behandeld in de les):
 --     CREATE OR REPLACE VIEW personeel AS
 -- 	     SELECT mnr, voorl, naam as medewerker, afd, functie
 --       FROM medewerkers;
+SELECT p.mnr, p.voorl, p.medewerker, p.afd, p.functie, d.cursus, d.begindatum, d.docent, d.locatie
+FROM personeel p
+         JOIN deelnemers d ON p.mnr = d.cursist;
+
 -- 3. Is de view "deelnemers" updatable ? Waarom ?
+-- nee omdat de view een combinatie is van twee kolommen (door de join) word het een stuk complexer om hem te updaten
 
 
 -- S6.2.
 --
 -- 1. Maak een view met de naam "dagcursussen". Deze view dient de gegevens op te halen: 
---      code, omschrijving en type uit de tabel curssussen met als voorwaarde dat de lengte = 1. Toon aan dat de view werkt. 
+--      code, omschrijving en type uit de tabel curssussen met als voorwaarde dat de lengte = 1. Toon aan dat de view werkt.
+create view dagcursussen as select code, omschrijving from cursussen where lengte = 1;
 -- 2. Maak een tweede view met de naam "daguitvoeringen". 
---    Deze view dient de uitvoeringsgegevens op te halen voor de "dagcurssussen" (gebruik ook de view "dagcursussen"). Toon aan dat de view werkt
--- 3. Verwijder de views en laat zien wat de verschillen zijn bij DROP view <viewnaam> CASCADE en bij DROP view <viewnaam> RESTRICT
+--    Deze view dient de uitvoeringsgegevens op te halen voor de "dagcurssussen" (gebruik ook de view "dagcursussen"). Toon aan dat de view werkt\
+CREATE OR REPLACE VIEW daguitvoeringen as select u.begindatum, u.docent ,u.locatie, dc.code, dc.omschrijving from uitvoeringen u join dagcursussen dc on u.cursus = dc.code;
 
+
+-- 3. Verwijder de views en laat zien wat de verschillen zijn bij DROP view <viewnaam> CASCADE en bij DROP view <viewnaam> RESTRICT
+-- drop view restrict verwijderd alleen de view als er niks aan afhankelijk is
+-- en de drop view cascade verwijderd de view en ook alle andere objecten die afhankelijk zijn zoals een andere view
